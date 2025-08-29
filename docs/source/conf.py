74b0c752a335c72b96ef2c5623f7131d6655f28d
chaleaoch <chaleaoch@gmail.com>
2025-08-29 13:55:03 +0800

    Update documentation for lumi_filter package
    - Added new HTML files for Python module index and search functionality.
    - Created search index JavaScript file for improved search capabilities.
    - Updated Sphinx configuration to reflect new project version and author information.
    - Revised index.rst to enhance structure and content presentation.
    - Expanded lumi_filter.rst to include detailed module documentation.
    - Removed obsolete modules.rst file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,17 +3,20 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
 
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 project = "lumi_filter"
-copyright = "2025, Your Name"
-author = "Your Name"
-release = "0.1"
+copyright = "2025, chaleaoch"
+author = "chaleaoch"
+
+version = "0.2"
+release = "0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,18 +24,15 @@ release = "0.1"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx_rtd_theme",
+    "sphinx.ext.viewcode",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
-language = "en"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_static_path = ["_static"]
-
-napoleon_google_docstring = True
